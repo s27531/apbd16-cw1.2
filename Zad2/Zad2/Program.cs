97e570ca63434376592c454eb8122d29fa80f35b
s27531 <s27531@pjwstk.edu.pl>
2025-03-13 15:52:45 +0100

    Added method Add().

--- Zad2/Zad2/Program.cs
+++ Zad2/Zad2/Program.cs
@@ -6,4 +6,9 @@
         Console.WriteLine("Very good program:");
         Console.WriteLine("==================");
     }
+
+    public static int Add(int a, int b)
+    {
+        return 0;
+    }
 }
